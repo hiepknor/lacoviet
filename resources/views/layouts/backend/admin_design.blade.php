5ef0abac43aad2e3dbc491337ae1d05519e91e81
Hiep Mai <hiepknor@gmail.com>
2019-09-23 03:30:59 +0700

    Update css for backend

--- resources/views/layouts/backend/admin_design.blade.php
+++ resources/views/layouts/backend/admin_design.blade.php
@@ -4,26 +4,26 @@
 <title>Laravel Admin</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap-responsive.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/uniform.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/select2.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/fullcalendar.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/matrix-style.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/matrix-media.css') }}" />
-<link rel="stylesheet" href="{{ asset('fonts/backend_fonts/css/font-awesome.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/jquery.gritter.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/bootstrap-responsive.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/uniform.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/select2.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/fullcalendar.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/matrix-style.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/matrix-media.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend/jquery.gritter.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/backend_fonts/css/font-awesome.css') }}" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-@include('layouts.adminLayout.admin_header')
+@include('layouts.backend.admin_header')
 
-@include('layouts.adminLayout.admin_sidebar')
+@include('layouts.backend.admin_sidebar')
 
 @yield('content')
 
-@include('layouts.adminLayout.admin_footer')
+@include('layouts.backend.admin_footer')
 
 <script src="{{ asset('js/backend_js/jquery.min.js') }}"></script> 
 <script src="{{ asset('js/backend_js/jquery.ui.custom.js') }}"></script> 
